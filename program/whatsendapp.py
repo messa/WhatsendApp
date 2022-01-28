2d744217da4cc4ba4f8f6fdbbd79d0047df8d97d
Jakub Kolář <78088608+Jakewh@users.noreply.github.com>
2022-01-28 12:29:31 +0100

    Update whatsendapp.py

--- program/whatsendapp.py
+++ program/whatsendapp.py
@@ -6,21 +6,22 @@ __        ___           _                      _    _
  \ \ /\ / /| '_ \ / _` | __/ __|/ _ \ '_ \ / _` | / _ \ | '_ \| '_ \ 
   \ V  V / | | | | (_| | |_\__ \  __/ | | | (_| |/ ___ \| |_) | |_) |
    \_/\_/  |_| |_|\__,_|\__|___/\___|_| |_|\__,_/_/   \_\ .__/| .__/ 
-                                                        |_|   |_|    V1
+                                                        |_|   |_|    V0.1
 """)
-print("Poznámka! Pro správnou funkčnost programu, musíte být přihlášeni k WhatsApp web")
+print("""
+Program slouží pro odeslání WhatsApp zprávy v určitý čas na česká telefonní čísla.
+Poznámka! Pro funkčnost programu, musíte být přihlášeni k WhatsApp web.
+https://web.whatsapp.com/
+""")
+
+
 
 tel = "+420" + input("Zadejte telefonní číslo příjemce: ")
 mes = input("Zadejte zprávu pro příjemce: ")
-h = input("Zadejte hodinu odeslání: ")
-m = input("Zadejte minutu odeslání: ")
-
-print(tel)
-
+print("Teď nastavíme čas odeslání zprávy")
+h = int(input("Zadejte pouze hodinu odeslání ve tvaru hh: "))
+m = int(input("Teď zadejte minuty ve tvaru mm: "))
 
 pywhatkit.sendwhatmsg(tel, mes, h, m)
-
-
-
 
 #pywhatkit.sendwhatmsg("+420775775488", "Test", 13, 5)
