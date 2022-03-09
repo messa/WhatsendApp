0f4de74544b1c3436227b02a8cb0fb49f02ba172
Petr Messner <petr@leadhub.co>
2022-03-09 14:25:47 +0100

    add label.pack()

--- program/whatsendapp.py
+++ program/whatsendapp.py
@@ -1,13 +1,13 @@
-import pywhatkit
+#import pywhatkit
 import tkinter as tk
 from tkinter import *
 import sys
 """
-__        ___           _                      _    _                
-\ \      / / |__   __ _| |_ ___  ___ _ __   __| |  / \   _ __  _ __  
- \ \ /\ / /| '_ \ / _` | __/ __|/ _ \ '_ \ / _` | / _ \ | '_ \| '_ \ 
+__        ___           _                      _    _
+\ \      / / |__   __ _| |_ ___  ___ _ __   __| |  / \   _ __  _ __
+ \ \ /\ / /| '_ \ / _` | __/ __|/ _ \ '_ \ / _` | / _ \ | '_ \| '_ \
   \ V  V / | | | | (_| | |_\__ \  __/ | | | (_| |/ ___ \| |_) | |_) |
-   \_/\_/  |_| |_|\__,_|\__|___/\___|_| |_|\__,_/_/   \_\ .__/| .__/ 
+   \_/\_/  |_| |_|\__,_|\__|___/\___|_| |_|\__,_/_/   \_\ .__/| .__/
                                                         |_|   |_|    V0.2
 """
 
@@ -26,8 +26,11 @@ def odeslat():
     z = zprava.get()
     h = int(hodina.get())
     m = int(minuta.get())
-    Label(top, text=pywhatkit.sendwhatmsg(c, z, h, m))
-    
+    #text = pywhatkit.sendwhatmsg(c, z, h, m)
+    text = 'test test'
+    Label(top, text=text).pack()
+
+
     #pywhatkit.sendwhatmsg("+420775775488", "Test", 13, 5)
 
 # tel číslo
